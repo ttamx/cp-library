093d28e0431c78e9417bd28a1ca655794eee4e12
ttamx <teetat.tata@gmail.com>
2024-09-21 22:47:38 +0700

    more monoids

---
+++ group/monoid-action/min-idx-add.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "group/monoid/add.hpp"
+#include "group/monoid/min-idx.hpp"
+
+/**
+ * Author: Teetat T.
+ * Date: 2024-04-14
+ * Description: Add to Min Action class.
+ */
+
+template<class T>
+struct MinAddIdxAction{
+    using InfoMonoid = MinIdxMonoid<T>;
+    using TagMonoid = AddMonoid<T>;
+    using Info = typename InfoMonoid::value_type;
+    using Tag = typename TagMonoid::value_type;
+    static constexpr Info op(const Info &a,const Tag &b){
+        return (a==InfoMonoid::unit()?a:Info(a.first+b,a.second));
+    }
+};
+
