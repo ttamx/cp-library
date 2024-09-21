47447f81bbeef923745ffc13f3289b8f7d1f2c4d
ttamx <teetat.tata@gmail.com>
2024-09-21 23:25:43 +0700

    feat: floor sum function

---
+++ verify/yosupo/number-theory/sum_of_floor_of_linear.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sum_of_floor_of_linear"
+#include "template.hpp"
+#include "number-theory/floor-sum.hpp"
+
+int main(){
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while(t--){
+        ll n,m,a,b;
+        cin >> n >> m >> a >> b;
+        cout << floor_sum(a,b,m,n-1) << "\n";
+    }
+}
